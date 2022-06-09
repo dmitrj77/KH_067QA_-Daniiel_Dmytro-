d3be1d0c98f6d28f2eb595c4ade34e9520237218
Daniel Dmitry <dmitrj77@gmail.com>
2022-06-09 12:46:51 +0300

    add delete product from cart action

--- KH_067QA_-Daniiel_Dmytro/src/main/java/task1/Main.java
+++ KH_067QA_-Daniiel_Dmytro/src/main/java/task1/Main.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+       // System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.google.com/");
